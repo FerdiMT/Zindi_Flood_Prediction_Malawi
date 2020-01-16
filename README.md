6061d73f140e87e48115c8f56f484322bfe79ce3
Martí Torrent <ferran.marti.torrent@accenture.com>
2020-01-16 17:56:58 +0100

    id_0, baseline for ML

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Zindi_Flood_Prediction_Malawi
 https://zindi.africa/competitions/2030-vision-flood-prediction-in-malawi/
+
+#### Changelog:
+
+
+
+| ID   | Version     | Comments | Zindi score | Issues/Todo's                                                |
+| ---- | ----------- | -------- | ----------- | ------------------------------------------------------------ |
+| id_0 | ML-Baseline | Baseline | 0.15528     | Some negative scores (shouldn't be there)<br />Nothing done with coordinates (important?)<br />Feature creation with the evolution of rain<br />Standarization of rain volume for both periods? |
+
---
+++ src/ML_approach.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from sklearn.pipeline import Pipeline
+import xgboost as xgb
+from sklearn.model_selection import GridSearchCV
+
+directory='data/'
+
+# Loading the data
+df = pd.read_csv(directory + 'Train.csv')
+sample_submission = pd.read_csv(directory + 'SampleSubmission.csv')
+
+# DATA WRANGLING
+# Keeping the square ids labels for submitting results.
+square_ids = df[['Square_ID']]
+# Creating a train and a test dataset
+df.set_index('Square_ID', inplace=True)
+columns_names = list(df.columns)
+
+# Separating values for train (with target) and test sets.
+colnames_train = ['target_2015','X', 'Y', 'LC_Type1_mode', 'elevation',
+                  'precip 2014-11-16 - 2014-11-23', 'precip 2014-11-23 - 2014-11-30', 'precip 2014-11-30 - 2014-12-07',
+                  'precip 2014-12-07 - 2014-12-14', 'precip 2014-12-14 - 2014-12-21', 'precip 2014-12-21 - 2014-12-28',
+                  'precip 2014-12-28 - 2015-01-04', 'precip 2015-01-04 - 2015-01-11', 'precip 2015-01-11 - 2015-01-18',
+                  'precip 2015-01-18 - 2015-01-25', 'precip 2015-01-25 - 2015-02-01', 'precip 2015-02-01 - 2015-02-08',
+                  'precip 2015-02-08 - 2015-02-15', 'precip 2015-02-15 - 2015-02-22', 'precip 2015-02-22 - 2015-03-01',
+                  'precip 2015-03-01 - 2015-03-08', 'precip 2015-03-08 - 2015-03-15']
+
+colnames_test = ['X', 'Y', 'LC_Type1_mode', 'elevation',
+                 'precip 2019-01-20 - 2019-01-27', 'precip 2019-01-27 - 2019-02-03', 'precip 2019-02-03 - 2019-02-10',
+                 'precip 2019-02-10 - 2019-02-17', 'precip 2019-02-17 - 2019-02-24', 'precip 2019-02-24 - 2019-03-03',
+                 'precip 2019-03-03 - 2019-03-10', 'precip 2019-03-10 - 2019-03-17', 'precip 2019-03-17 - 2019-03-24',
+                 'precip 2019-03-24 - 2019-03-31', 'precip 2019-03-31 - 2019-04-07', 'precip 2019-04-07 - 2019-04-14',
+                 'precip 2019-04-14 - 2019-04-21', 'precip 2019-04-21 - 2019-04-28', 'precip 2019-04-28 - 2019-05-05',
+                 'precip 2019-05-05 - 2019-05-12', 'precip 2019-05-12 - 2019-05-19']
+
+train = df.loc[:,colnames_train]
+test = df.loc[:,colnames_test]
+
+# Renaming precipitation columns to -17weeks until -1week (prior to the flooding).
+train.columns = ['target_2015','X', 'Y', 'LC_Type1_mode', 'elevation',
+                 '-17_weeks', '-16_weeks','-15_weeks', '-14_weeks', '-13_weeks', '-12_weeks',
+                 '-11_weeks', '-10_weeks','-9_weeks', '-8_weeks', '-7_weeks', '-6_weeks',
+                 '-5_weeks', '-4_weeks','-3_weeks', '-2_weeks', '-1_weeks']
+
+test.columns = ['X', 'Y', 'LC_Type1_mode', 'elevation',
+                '-17_weeks', '-16_weeks', '-15_weeks', '-14_weeks', '-13_weeks', '-12_weeks',
+                '-11_weeks', '-10_weeks', '-9_weeks', '-8_weeks', '-7_weeks', '-6_weeks',
+                '-5_weeks', '-4_weeks', '-3_weeks', '-2_weeks', '-1_weeks']
+
+# ML MODEL
+# TODO: For now we train on all the training dataset, we don't do a train-test split.
+
+# Create Pipeline
+pipeline_xgb =Pipeline(
+    [('xgb', xgb.XGBRegressor())
+     ])
+
+# Set the parameters
+params={}
+params['xgb__learning_rate'] = [0.05, 0.08]
+params['xgb__objective'] = ['reg:squarederror']
+params['xgb__max_depth'] = [3, 5]
+
+# GridSearch
+CV = GridSearchCV(pipeline_xgb, params, scoring = 'neg_mean_squared_error', n_jobs= 1, cv=3)
+CV.fit(train.drop(['X', 'Y', 'target_2015'], axis=1), train['target_2015'])
+
+
+# TEST SUBMISSION
+
+predictions = CV.predict(test.drop(['X', 'Y'], axis=1))
+predictions = pd.DataFrame(predictions)
+
+# Remove the negative predictions (there shouldn't be any...) and put them to 0.
+predictions[predictions < 0] = 0
+
+# Redo the predictions dataframe into the right format to submit.
+predictions.reset_index(inplace=True)
+submission = square_ids.join(predictions)
+submission.columns = ['Square_ID', 'target_2019']
+
+submission.to_csv('submission_ML.csv', index=False)
+
+
+# TODO:ISSUES. Negative Predictions!
